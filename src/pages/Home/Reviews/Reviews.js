f5f5e11e2576c5d139e331e7b0a0077b77d9a118
solaiman15-11026 <samimunna49@gmail.com>
2022-05-09 19:59:20 +0600

    all

--- src/pages/Home/Reviews/Reviews.js
+++ src/pages/Home/Reviews/Reviews.js
@@ -6,61 +6,60 @@ import './Reviews.css'
 const Reviews = () => {
     return (
         < div className='my-5 container pb-4 bg-color' >
-            <h1 className='mt-5 text-white pt-5 mb-2 text-center text-uppercase '>What our Customer Saying</h1>
+            <h1 className='mt-5 text-primary pt-5 mb-2 text-center text-uppercase '>Reviews</h1>
             <hr style={{ width: '35%', margin: '0  auto' }} className='mb-4' />
             <div className="card-group text-center">
                 <div className="card m-3 border-0 shadow rounded">
-                    <img style={{ width: "150px" }} className="card-img-top mx-auto rounded-circle mt-3" src="https://i.ibb.co/1KCJQ9R/arif.jpg" alt="Card image cap" />
+                    <img style={{ width: "200px" }} className="card-img-top mx-auto rounded-circle mt-3" src="https://i.ibb.co/wr33zYt/francis-angelyn-logo2020.jpg" alt="Card image cap" />
                     <div className="card-body">
-                        <h3 className="card-title">Md Arif Hossain</h3>
-                        <FontAwesomeIcon className='fs-2 text-secondary me-3' icon={faQuoteLeft}></FontAwesomeIcon>
-                        <span className="card-text">Buying experience is awesome and Dealer also helped us to find the correct model as per requirement. The riding experience is just freaking fantastic and ride quality is awesome as well.</span>
-                        <FontAwesomeIcon className='fs-2 text-secondary ms-3' icon={faQuoteRight}></FontAwesomeIcon>
+                        <h3 className="card-title">Risa Leo</h3>
+                        <p className='reviwe'>
+                            This information is mostly requested in an employment biodata. Employers usually ask applicants to highlight relevant skills in a job application.
+                        </p>
                     </div>
                     <div className="card-footer">
                         <small className="text-muted">
-                            <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
-                            <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
-                            <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
-                            <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
-                            <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
+                            <FontAwesomeIcon className='text-primary' icon={faStar}></FontAwesomeIcon>
+                            <FontAwesomeIcon className='text-primary' icon={faStar}></FontAwesomeIcon>
+                            <FontAwesomeIcon className='text-primary' icon={faStar}></FontAwesomeIcon>
+                            <FontAwesomeIcon className='text-primary' icon={faStar}></FontAwesomeIcon>
+                            <FontAwesomeIcon className='text-primary' icon={faStar}></FontAwesomeIcon>
                         </small>
                     </div>
                 </div>
                 <div className="card m-3 border-0 shadow rounded">
-                    <img style={{ width: "150px" }} className="card-img-top mx-auto rounded-circle mt-3" src="https://i.ibb.co/LhQC3rY/jilani.jpg" alt="Card image cap" />
+                    <img style={{ width: "213px" }} className="card-img-top mx-auto rounded-circle mt-3" src="https://i.ibb.co/qg5j58J/istockphoto-1148380353-612x612.jpg" alt="Card image cap" />
                     <div className="card-body">
-                        <h3 className="card-title">Ahmed Akash</h3>
-                        <FontAwesomeIcon className='fs-2 text-secondary me-3' icon={faQuoteLeft}></FontAwesomeIcon>
-                        <span className="card-text">I driven this car and feels awesome and comfortable drive, Mileage is superb, really value for money. Even in long drive can't feel tired of sitting and driving. Love to buy this car as my new upcoming car.</span>
-                        <FontAwesomeIcon className='fs-2 text-secondary ms-3' icon={faQuoteRight}></FontAwesomeIcon>
+                        <h3 className="card-title">David John</h3>
+                        <p className='reviwe'>
+                            This information is mostly requested in an employment biodata. Employers usually ask applicants to highlight relevant skills in a job application.
+                        </p>
                     </div>
                     <div className="card-footer">
                         <small className="text-muted">
-                            <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
-                            <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
-                            <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
-                            <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
-                            <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
+                            <FontAwesomeIcon className='text-primary' icon={faStar}></FontAwesomeIcon>
+                            <FontAwesomeIcon className='text-primary' icon={faStar}></FontAwesomeIcon>
+                            <FontAwesomeIcon className='text-primary' icon={faStar}></FontAwesomeIcon>
+                            <FontAwesomeIcon className='text-primary' icon={faStar}></FontAwesomeIcon>
+                            <FontAwesomeIcon className='text-primary' icon={faStar}></FontAwesomeIcon>
                         </small>
                     </div>
                 </div>
                 <div className="card m-3 border-0 shadow rounded">
-                    <img style={{ width: "150px" }} className="card-img-top mx-auto rounded-circle mt-3" src="https://i.ibb.co/fCJKKMq/prince.jpg" alt="Card image cap" />
+                    <img style={{ width: "213px" }} className="card-img-top mx-auto rounded-circle mt-3" src="https://i.ibb.co/sKBdJg6/photo-1438761681033-6461ffad8d80.jpg" alt="Card image cap" />
                     <div className="card-body">
-                        <h3 className="card-title">Hamidul Islam</h3>
-
-                        <FontAwesomeIcon className='fs-2 text-secondary me-3' icon={faQuoteLeft}></FontAwesomeIcon>
-                        <span className="card-text"> have drive my friend's car and believe me the experience was fantastic. Silent Car though powerful. Interiors are just so rich and decent. Exterior is just awesome. Definitely a tough competition to Honda City.</span>
-                        <FontAwesomeIcon className='fs-2 text-secondary ms-3' icon={faQuoteRight}></FontAwesomeIcon>
+                        <h3 className="card-title">Ema Jahan</h3>
+                        <p className='reviwe'>
+                            This information is mostly requested in an employment biodata. Employers usually ask applicants to highlight relevant skills in a job application.
+                        </p>
                     </div>
                     <div className="card-footer">
                         <small className="text-muted">
-                            <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
-                            <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
-                            <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
-                            <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
-                            <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
+                            <FontAwesomeIcon className='text-primary' icon={faStar}></FontAwesomeIcon>
+                            <FontAwesomeIcon className='text-primary' icon={faStar}></FontAwesomeIcon>
+                            <FontAwesomeIcon className='text-primary' icon={faStar}></FontAwesomeIcon>
+                            <FontAwesomeIcon className='text-primary' icon={faStar}></FontAwesomeIcon>
+                            <FontAwesomeIcon className='text-primary' icon={faStar}></FontAwesomeIcon>
                         </small>
                     </div>
                 </div>
