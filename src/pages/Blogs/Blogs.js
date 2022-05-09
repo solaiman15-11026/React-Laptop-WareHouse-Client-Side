bfc2ea684ad92cb0d34d5a16b3075a3a202d5c3e
solaiman15-11026 <samimunna49@gmail.com>
2022-05-09 19:07:24 +0600

    new

--- src/pages/Blogs/Blogs.js
+++ src/pages/Blogs/Blogs.js
@@ -1,44 +1,34 @@
 import React from 'react';
+import './Blogs.css'
 
 const Blogs = () => {
     return (
         <div className='container '>
-            <h1 className='mt-4 text-uppercase text-primary text-center'>Blogs</h1>
+            <h1 className=' mt-4 text-uppercase text-danger text-center'>Blogs</h1>
 
             <div>
-                <div className='bg-light blog-container py-3 mt-5'>
-                    <h3 className='py-2 text-uppercase text-center text-success'> Javascript VS Nodejs</h3>
-                    <p className='pb-2 text-center rr'>JavaScript is a scripting language that may be used in web browsers.
-                        Node.js, on the other hand, is a JavaScript interpreter or execution environment that includes a large number of necessary libraries and other components.
-                        JavaScript is a single defining programming language that can operate in any browser using the default browser environment. It's a powerful language that's typically utilized in a web application for any type of verification or business logic.
-                        Node.js also contains a large number of related libraries, which we often utilize in javascript for general-purpose programming.
-                        It's a type of interpreter or environment that can represent JavaScript and run any JavaScript program. It primarily assists us in performing non-blocking operations such as retrieving operating system specific information such as certificate or hardware details;
-                        we can utilize node js to accomplish this, but JavaScript standard programming will not.
+                <div className='p-4 mt-4 shadow rounded-3'>
+                    <h3 className='py-2  text-center text-primary'>Different Between Javascript VS Nodejs?</h3>
+                    <p className='pb-2 text-center rr'>NodeJS is a cross-platform, open-source Javascript runtime environment that enables server-side javascript execution. Javascript code can now execute outside of the browser thanks to Nodejs. Nodejs has a large number of modules and is primarily used in web development.<br />
+                        A scripting language is Javascript. JS is the most common abbreviation. Javascript might be considered an updated version of the ECMA script. Javascript is a high-level programming language that is built on prototype inheritance and employs the Oops notion.
                     </p>
                 </div>
-                <div className='bg-light blog-container py-3 mt-5'>
-                    <h3 className='py-2 text-uppercase text-center text-success'>  When should you use nodejs and when should you use mongodb</h3>
-                    <p className='pb-2 text-center rr'>Nodejs is a Javascript engine that can be used to create any application (by programming in the Javascript language).
-                        It executes Javascript code. It is most typically used to create servers that can answer to web requests, but it may also be used to create a variety of other types of programming.
-                        MongoDB is a database management system. MongoDB is a database that is used by code within an application or server to save, query, and update data.
-                        Many web servers are developed with Nodejs and then utilize MongoDB to store data.
+                <div className='p-4 mt-4 shadow rounded-3'>
+                    <h3 className='py-2 text-center text-primary'>  When should you use mongodb and when should you use nodejs?</h3>
+                    <p className='pb-2 text-center rr'>Node.js is an open-source and cross-platform runtime environment built on Chrome's V8 JavaScript engine for executing JavaScript code outside of a browser. You need to recollect that NodeJS isn't a framework, and it's not a programing language. It provides an event-driven, non-blocking (asynchronous) I/O and cross-platform runtime environment for building highly scalable server-side applications using JavaScript. <br />
+                        MongoDB can be used in places that require simple queries, easy and fast integration of data and have data whose structure changes constantly with time.
                     </p>
                 </div>
-                <div className='bg-light blog-container py-3 mt-5'>
-                    <h3 className=' py-3 text-uppercase text-center text-success' > Sql VS Nosql </h3>
-                    <p className='pb-3 text-center rr'>Relational databases are SQL, while non-relational databases are NoSQL.
-                        SQL databases have a specified schema and employ structured query language. For unstructured data, NoSQL databases use dynamic schemas.
-                        SQL databases scale vertically, but NoSQL databases scale horizontally.
-                        NoSQL databases are document, key-value, graph, or wide-column stores, whereas SQL databases are table-based.
-                        SQL databases excel in multi-row transactions, while NoSQL excels at unstructured data such as documents and JSON.
+                <div className=' p-4 mt-4 shadow rounded-3'>
+                    <h3 className=' py-3  text-center text-primary' >Different Between  Sql VS Nosql? </h3>
+                    <p className='pb-3 text-center rr'>SQL refers to relational databases, whereas NoSQL refers to non-relational databases. SQL databases use structured query language and have a defined schema. NoSQL databases use dynamic schemas for unstructured data. NoSQL databases scale horizontally, whereas SQL databases scale vertically. SQL databases are table-based, whereas NoSQL databases are document, key-value, graph, or wide-column stores. Multi-row transactions are a strength of SQL databases,
+                        whereas unstructured data like documents and JSON is a strength of NoSQL databases.
                     </p>
                 </div>
-                <div className='bg-light blog-container py-3 my-5'>
-                    <h3 className='py-2  text-uppercase text-center text-success'> What is the purpose of jwt and how does it work?</h3>
-                    <p className='pb-2 text-center'>JSON Web Token (JWT) is an open standard (RFC 7519) that specifies a compact and self-contained method for securely communicating information as a JSON object between two parties.
-                        Because it is digitally signed, this information can be checked and trusted. JWTs can be signed using a secret (using the HMAC algorithm) or with an RSA or ECDSA public/private key combination.
-                        Although JWTs can be encrypted to guarantee party-to-party confidentiality, we will concentrate on signed tokens. Signed tokens can be used to validate the validity of the claims they contain,
-                        whilst encrypted tokens keep those claims hidden from third parties. When public/private key pairings are used to sign tokens, the signature additionally verifies that only the party with the private key signed it.
+                <div className='p-4 mt-4 shadow rounded-3'>
+                    <h3 className='py-2  text-center text-primary'> What is the purpose of jwt and how does it work?</h3>
+                    <p className='pb-2 text-center rr'>JWT (JSON Web Token) is an open standard that allows two parties — a client and a server — to share security information. JSON items, containing a set of claims, are encoded in each JWT. JWTs use a cryptographic technique to ensure that the claims cannot be changed after the token has been issued. <br />
+                        Authentication is the primary use of JWT. When a user logs in to an application, the app generates a JWT and sends it to the user. The JWT will be included in the user's subsequent requests. The token informs the server about the routes, services, and resources that the user is permitted to use.
                     </p>
                 </div>
             </div>
